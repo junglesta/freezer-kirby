5faca3c0114ff10f66c7541b44c59b14a43867e2
toybreaker <rokmabox@gmail.com>
2017-08-06 15:25:51 +0800

    feed 100 posts

--- site/templates/feed.php
+++ site/templates/feed.php
@@ -1,6 +1,6 @@
 <?php
 
-echo page('projects')->children()->visible()->flip()->limit(50)->feed(array(
+echo page('projects')->children()->visible()->flip()->limit(100)->feed(array(
   'title'       => $page->title(),
   'description' => $page->description(),
   'link'        => 'projects',
